c9c5ff2dc0c4d3bd8cb041bfec75ee6e0619695e
shahad <shahadottt@gmail.com>
2022-12-17 21:30:13 +0300

    refactor: add print hello  message

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -16,6 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = input('Enter the city (chicago, new york city, washington) : ')
     city = city.casefold()
